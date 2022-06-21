55ed1a8cb41821fead2a120cd297168a56f77b67
muralidhar78 <muralidhar78@gmail.com>
2022-06-21 15:41:29 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.header( 'Breakfast Favorites')
+streamlit.header('🥣 Breakfast Favorites')
 streamlit.text(' omega 3 & Blueberry Oatmeal')
-streamlit.text('@ Kale, Spinach & Rocket Smoothie')
-streamlit.text('4 Hard-Boiled Free-Range Egg')
-streamlit. text("O Avocado Toast")
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit. text("🥑🍞Avocado Toast")
